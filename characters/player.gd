ac7f5473c77af9046456696b7032b6dfcfe8d633
Salva Romero <salvaromero08@gmail.com>
2023-03-11 15:34:09 +0100

    se puede redimensionar

--- characters/player.gd
+++ characters/player.gd
@@ -3,13 +3,14 @@ extends CharacterBody2D
 
 const SPEED = 200.0
 const JUMP_VELOCITY = -300.0
+var is_climbing = false
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 @onready var animated_sprite = $AnimatedSprite2D
 @onready var right_wall = $RightWall
-@onready var left_wall = $LeftWallvar 
-rng = RandomNumberGenerator.new()
+@onready var left_wall = $LeftWall
+var rng = RandomNumberGenerator.new()
 
 func _physics_process(delta): 
 	
@@ -21,15 +22,16 @@ func _physics_process(delta):
 		velocity.y += gravity * delta
 		
 		if (velocity.y <= 0):
-			animated_sprite.animation = "jump"
+			animated_sprite.play("jump")
 		else:
-			animated_sprite.animation = "fall"
+			animated_sprite.play("fall")
 	else:
 
-		if (velocity.x == 0):
-			animated_sprite.play("idle")
-		else:
-			animated_sprite.play("run")
+		if not is_climbing:
+			if (velocity.x == 0):
+				animated_sprite.play("idle")
+			else:
+				animated_sprite.play("run")
 
 	# Handle Jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
@@ -55,7 +57,13 @@ func _physics_process(delta):
 			animated_sprite.flip_h = false
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+		
+	if (Input.is_action_pressed("climb") and nextToWall()):
+		animated_sprite.play("hit")
+		is_climbing = true
+	else:
+		is_climbing = false
+		
 	move_and_slide()
 	
 func nextToWall():
