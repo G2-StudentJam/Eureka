132cee119d33c8a852f27c76f412fdfb64594663
Salva Romero <salvaromero08@gmail.com>
2023-03-12 13:20:23 +0100

    tanque recibe items

--- characters/player.gd
+++ characters/player.gd
@@ -13,7 +13,14 @@ var current_animation = "idle"
 var animation_direction = "right"
 var stamina = MAX_STAMINA
 
+var item_inventory = {
+	"wrench": false,
+	"screw": true,
+	"nut": false
+}
+
 signal stamina_changed(new_value)
+signal item_dropped(item)
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -25,6 +32,7 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 @onready var top_left_wall = $TopLeftWall
 @onready var stamina_bar = $Stamina/StaminaBar
 @onready var stamina_show_timer = $StaminaShowTimer
+@onready var tank = get_parent().get_node("Tank")
 
 
 var rng = RandomNumberGenerator.new()
@@ -146,7 +154,6 @@ func wall_climb(delta):
 	if (Input.is_action_pressed("climb") and nextToWall() and stamina > 0 and $CanvasLayer/Guantes.visible):
 		if current_animation != "climb":
 			#starts climbing
-			print("empieza a escalar")
 			current_animation = "climb"
 		set_stamina(stamina - STAMINA_DEPLETION_SPEED * delta)
 		is_climbing = true
@@ -158,3 +165,9 @@ func wall_climb(delta):
 		if aboutToFinishClimb():
 			jump(0.6,false)
 
+
+func _on_tank_player_entered():
+	for item in item_inventory:
+		if item_inventory[item]:
+			item_dropped.emit(item)
+			item_inventory[item] = false
