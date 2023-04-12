cc08977d5a2922c14071d4602e9d1d5f2d6025f8
Salva Romero <salvaromero08@gmail.com>
2023-04-12 19:40:17 +0200

    pincho reworked

--- elements/pincho.gd
+++ elements/pincho.gd
@@ -2,14 +2,21 @@ extends Node2D
 
 signal player_hit_spikes
 
-@export var offset = Vector2(0, 10)
+@export var is_moving : bool
+@export var offset = Vector2(0, -10)
 @export var duration = 3.0
 
 func _ready():
-	start_tween()
+	if is_moving:
+		start_tween()
 
 func start_tween():
 	var tween = get_tree().create_tween().set_process_mode(Tween.TWEEN_PROCESS_PHYSICS)
 	tween.set_loops().set_parallel(false)
 	tween.tween_property($AnimatableBody2D, "position", offset, duration / 2)
 	tween.tween_property($AnimatableBody2D, "position", Vector2.ZERO, duration / 2)
+
+
+func _on_body_entered(body):
+	if body.get("name") == "Player":
+		player_hit_spikes.emit()
