82da75c8ea2b64453126a147d25d880f6625e95d
Sergio <2001sergiobr@gmail.com>
2023-04-12 20:55:04 +0200

    Vi? More like VIrgin

--- elements/pincho.gd
+++ elements/pincho.gd
@@ -4,7 +4,8 @@ signal player_hit_spikes
 
 @export var is_moving : bool
 @export var offset = Vector2(0, -10)
-@export var duration = 3.0
+@export var duration = 1
+@export var waiting = 1
 
 func _ready():
 	if is_moving:
@@ -14,7 +15,9 @@ func start_tween():
 	var tween = get_tree().create_tween().set_process_mode(Tween.TWEEN_PROCESS_PHYSICS)
 	tween.set_loops().set_parallel(false)
 	tween.tween_property($AnimatableBody2D, "position", offset, duration / 2)
+	tween.tween_property($AnimatableBody2D, "position", offset, waiting)
 	tween.tween_property($AnimatableBody2D, "position", Vector2.ZERO, duration / 2)
+	tween.tween_property($AnimatableBody2D, "position", Vector2.ZERO, waiting)
 
 
 func _on_body_entered(body):
