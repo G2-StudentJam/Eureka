68e156e2b9996c879f6b95a594fcd4651d4810f3
Sergio <2001sergiobr@gmail.com>
2023-04-13 23:48:22 +0200

    casi_se_me_olvida

--- Scripts/Paracaidas.gd
+++ Scripts/Paracaidas.gd
@@ -2,7 +2,24 @@ extends Area2D
 
 signal paracas_recogido
 
+
+const ANIMATION_SPEED = 5
+var animation_direction = -1
+
+
 func _on_body_entered(body):
 	if (body.get_name() == "Player"):
 		paracas_recogido.emit()
 		queue_free()
+
+
+
+func _process(delta):
+	
+	position.y += ANIMATION_SPEED * delta * animation_direction
+	
+	
+
+
+func _on_timer_timeout():
+	animation_direction *= -1
